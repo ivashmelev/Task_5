e742c406e4638ed8bf33e62031e90ba05d207673
ishmelev <ishmelev@1ditis.ru>
2018-06-14 01:08:45 +0300

    work

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Task_4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(776, 544);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 566);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "MainForm";
+            this.Text = "Dispatcher Services";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.ServiceProcess;
+
+namespace Task_4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            ServiceController[] services;
+            services = ServiceController.GetServices();
+            for (int i = 0; i < services.Length; i++)
+            {
+                var count = services.Length;
+                checkedListBox1.Items.Add(services[i].ServiceName);
+                if (services[i].Status == ServiceControllerStatus.Running)
+                {
+                    checkedListBox1.SetItemChecked(i, true);
+                }
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckedListBox check = (CheckedListBox)sender;
+            if (check.GetItemCheckState)
+            {
+                MessageBox.Show("True");
+            }
+            else
+            {
+                MessageBox.Show("False");
+            }
+        }
+    } 
+}
